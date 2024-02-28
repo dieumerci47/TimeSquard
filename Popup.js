00f5e9dea5882499f9b6539378d55de91d4921bb
dieumerci <dieumercitsimba04@gmail.com>
2024-02-28 19:21:35 +0100

    Modification des fichers Popup Afin d'afficher le popup qui permet de partager le score

--- Popup.js
+++ Popup.js
@@ -1,51 +1,24 @@
-/*********************************************************************************
- *
- * Ce fichier contient toutes les fonctions nécessaires à l'affichage et à la
- * fermeture de la popup de partage.
- *
- *********************************************************************************/
+// Sélection du bouton "Partager" dans la zonePartage
+let btnPartage = document.querySelector(".zonePartage button");
 
-/**
- * Cette fonction affiche la popup pour partager son score.
- */
-function afficherPopup() {
-  let popupBackground = document.querySelector(".popupBackground");
-  // La popup est masquée par défaut (display:none), ajouter la classe "active"
-  // va changer son display et la rendre visible.
+// Sélection de la div popupBackground
+let popupBackground = document.querySelector(".popupBackground");
+
+// Ajout d'un écouteur d'événement sur le bouton "Partager"
+btnPartage.addEventListener("click", (event) => {
+  // Empêche la propagation de l'événement de clic sur le bouton partagé
+  event.stopPropagation();
+
+  // Affiche la popupBackground en ajoutant la classe "active"
   popupBackground.classList.add("active");
-}
+});
 
-/**
- * Cette fonction cache la popup pour partager son score.
- */
-function cacherPopup() {
-  let popupBackground = document.querySelector(".popupBackground");
-  // La popup est masquée par défaut (display:none), supprimer la classe "active"
-  // va rétablir cet affichage par défaut.
-  popupBackground.classList.remove("active");
-}
-
-/**
- * Cette fonction initialise les écouteurs d'événements qui concernent
- * l'affichage de la popup.
- */
-function initAddEventListenerPopup() {
-  // On écoute le click sur le bouton "partager"
-  let btnPartage = document.querySelector(".zonePartage button");
-  let popupBackground = document.querySelector(".popupBackground");
-  btnPartage.addEventListener("click", () => {
-    // Quand on a cliqué sur le bouton partagé, on affiche la popup
-    afficherPopup();
-  });
-
-  // On écoute le click sur la div "popupBackground"
-  popupBackground.addEventListener("click", (event) => {
-    // Si on a cliqué précisément sur la popupBackground
-    // (et pas un autre élément qui se trouve dedant)
-    if (event.target === popupBackground) {
-      // Alors on cache la popup
-      cacherPopup();
-    }
-  });
-}
-initAddEventListenerPopup();
+// Ajout d'un écouteur d'événement sur le document
+document.addEventListener("click", (event) => {
+  // Si l'événement de clic provient de la popupBackground
+  if (event.target === popupBackground) {
+    // Cela signifie que l'utilisateur a cliqué en dehors du contenu de la popup
+    // Donc on cache la popupBackground en enlevant la classe "active"
+    popupBackground.classList.remove("active");
+  }
+});
